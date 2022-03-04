34e4d98e79a3fbf8c53b34d1fef013cdc6d09c2b
ChrBondi <42577769+ChrBondi@users.noreply.github.com>
2022-03-04 14:26:27 +0100

    Kotlin cleanup

--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -2,18 +2,18 @@ fun main(args: Array<String>) {
     val constant_runtime = benchmark(::constants)
     val deadvariable_runtime = benchmark(::deadVariable)
     val functionmap_runtime = benchmark(::functionMap)
-    val letmap1_runtime = benchmark(::letMap)
-    val letmap2_runtime = benchmark(::letMap2)
-    val purevar1_runtime = benchmark(::getVarPure)
-    val purevar2_runtime = benchmark(::getVarPure)
+    val letmapconstant_runtime = benchmark(::letMapConstant)
+    val letmapadd_runtime = benchmark(::letMapAdd)
+    val get_var_pure_runtime = benchmark(::getVarPure)
+    val get_var_impure_runtime = benchmark(::getVarImpure)
     println()
     print_benchmark("Constant", constant_runtime)
     print_benchmark("DeadVariable", deadvariable_runtime)
     print_benchmark("FunctionMap", functionmap_runtime)
-    print_benchmark("LetMap1", letmap1_runtime)
-    print_benchmark("LetMap2", letmap2_runtime)
-    print_benchmark("PureVar1", purevar1_runtime)
-    print_benchmark("PureVar2", purevar2_runtime)
+    print_benchmark("LetMapConstant", letmapconstant_runtime)
+    print_benchmark("LetMapAdd", letmapadd_runtime)
+    print_benchmark("GetVarPure", get_var_pure_runtime)
+    print_benchmark("GetVarImpure", get_var_impure_runtime)
 }
 
 fun print_benchmark(name: String, runtime: Triple<Long, Long, Long>) {
@@ -35,7 +35,7 @@ fun benchmark(f: (((List<Int>) -> Unit ) -> Unit)): Triple<Long, Long, Long> {
 fun benchmark_ac(n: Int, f: ((List<Int>) -> Unit) -> Unit, acc: List<Long>): List<Long>  {
     if (n > 0) {
         val before = System.nanoTime()
-        f({ l: List<Int> -> {}})
+        f { l: List<Int> -> {} }
         val after = System.nanoTime()
         val time = (after - before)
         return benchmark_ac(n - 1, f, listOf(time) + acc)
@@ -53,27 +53,27 @@ fun constants(hof: (List<Int>) -> Unit)  {
     hof(if (!x) y else listOf(z,w))
 }
 
-fun deadVariable(hof: (List<Int>) -> Unit): Unit {
+fun deadVariable(hof: (List<Int>) -> Unit) {
     val x = List(1_000_000){7}
     hof(emptyList())
 }
 
 fun double(x: Int): Int = x*2
 
-fun functionMap(hof: (List<Int>) -> Unit): Unit {
+fun functionMap(hof: (List<Int>) -> Unit) {
     val list = List(1_000_000){7}
     val newList = list.map(::double)
     hof(newList)
 }
 
-fun letMap(hof: (List<Int>) -> Unit): Unit {
+fun letMapConstant(hof: (List<Int>) -> Unit) {
     val x = 5
     val list = List(1_000_000){7}
     val newList = list.map{y -> x+y}
     hof(newList)
 }
 
-fun letMap2(hof: (List<Int>) -> Unit): Unit {
+fun letMapAdd(hof: (List<Int>) -> Unit) {
     val x = 5+5
     val list =  List(1_000_000){7}
     val newList = list.map{y -> x+y}
@@ -86,14 +86,14 @@ fun getHello42(): Int  {
     return 42
 }
 
-fun getVarPure(hof: (List<Int>) -> Unit): Unit {
+fun getVarPure(hof: (List<Int>) -> Unit) {
     val x = get42()
     val list = List(1_000_000){7}
     val newList = list.map{y -> x + y}
     hof(newList)
 }
 
-fun getVarImpure(hof: (List<Int>) -> Unit): Unit {
+fun getVarImpure(hof: (List<Int>) -> Unit) {
     val x = getHello42()
     val list =  List(1_000_000){7}
     val newList = list.map{y -> x + y}
