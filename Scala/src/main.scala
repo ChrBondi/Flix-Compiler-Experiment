63391285697c13e50db858448db3eb363e4032c4
ChrBondi <42577769+ChrBondi@users.noreply.github.com>
2022-03-07 12:30:09 +0100

    new benchmarks in kotlin and scala

--- Scala/src/main.scala
+++ Scala/src/main.scala
@@ -7,6 +7,9 @@ object main {
     val letmapadd_runtime = benchmark(letMapAdd)
     val get_var_pure_runtime = benchmark(getVarPure)
     val get_var_impure_runtime = benchmark(getVarImpure)
+    val branch_pure = benchmark(branch_pure)
+    val branch_impure = benchmark(branch_impure)
+    val lambda = benchmark(lambda)
     println()
     print_benchmark("Constant", constant_runtime)
     print_benchmark("DeadVariable", deadvariable_runtime)
@@ -15,6 +18,9 @@ object main {
     print_benchmark("LetMapAdd", letmapadd_runtime)
     print_benchmark("GetVarPure", get_var_pure_runtime)
     print_benchmark("GetVarImpure", get_var_impure_runtime)
+    print_benchmark("BranchPure", branch_pure)
+    print_benchmark("BranchImpure", branch_impure)
+    print_benchmark("Lambda", lambda)
   }
 
   def print_benchmark(name: String, runtime: (Long, Long, Long)) {
@@ -42,65 +48,85 @@ object main {
     else acc
   }
 
-    def constants(hof: List[Int] => Unit): Unit = {
-      val x = true
-      val y = List.fill(1_000_000)(100)
-      val z = 3
-      val w = 4
-      hof(if (!x) y else List(z, w))
-    }
-
-    def deadVariable(hof: List[Int] => Unit): Unit = {
-      val x = List.fill(1000000)(7)
-      hof(List())
-    }
-
-    def double(x: Int): Int = x * 2
-
-    def functionMap(hof: List[Int] => Unit): Unit = {
-      val list = List.fill(1000000)(7)
-      val newList = list.map(double)
-      hof(newList)
-    }
-
-    def letMapConstant(hof: List[Int] => Unit): Unit = {
-      val x = 5
-      val list = List.fill(1000000)(7)
-      val newList = list.map(y => x + y)
-      hof(newList)
-    }
-
-    def letMapAdd(hof: List[Int] => Unit): Unit = {
-      val x = 5 + 5
-      val list = List.fill(1000000)(7)
-      val newList = list.map(y => x + y)
-      hof(newList)
-    }
-
-    def get42() = 42
-
-    def getHello42: Int = {
-      print("Hello")
-      42
-    }
-
-    def getVarPure(hof: List[Int] => Unit): Unit = {
-      val x = get42()
-      val list = List.fill(1000000)(7)
-      val newList = list.map(y => x + y)
-      hof(newList)
-    }
-
-    def getVarImpure(hof: List[Int] => Unit): Unit = {
-      val x = getHello42
-      val list = List.fill(1000000)(7)
-      val newList = list.map(y => x + y)
-      hof(newList)
-    }
-
-    def fib(n: Int): Int = {
-      if (n == 0) 0
-      else if (n == 1) 1
-      else fib(n - 2) + fib(n - 1)
-    }
+  def constants(hof: List[Int] => Unit): Unit = {
+    val x = true
+    val y = List.fill(1_000_000)(100)
+    val z = 3
+    val w = 4
+    hof(if (!x) y else List(z, w))
   }
+
+  def deadVariable(hof: List[Int] => Unit): Unit = {
+    val x = List.fill(1000000)(7)
+    hof(List())
+  }
+
+  def double(x: Int): Int = x * 2
+
+  def functionMap(hof: List[Int] => Unit): Unit = {
+    val list = List.fill(1000000)(7)
+    val newList = list.map(double)
+    hof(newList)
+  }
+
+  def letMapConstant(hof: List[Int] => Unit): Unit = {
+    val x = 5
+    val list = List.fill(1000000)(7)
+    val newList = list.map(y => x + y)
+    hof(newList)
+  }
+
+  def letMapAdd(hof: List[Int] => Unit): Unit = {
+    val x = 5 + 5
+    val list = List.fill(1000000)(7)
+    val newList = list.map(y => x + y)
+    hof(newList)
+  }
+
+  def get42() = 42
+
+  def getHello42: Int = {
+    print("Hello")
+    42
+  }
+
+  def getVarPure(hof: List[Int] => Unit): Unit = {
+    val x = get42()
+    val list = List.fill(1000000)(7)
+    val newList = list.map(y => x + y)
+    hof(newList)
+  }
+
+  def getVarImpure(hof: List[Int] => Unit): Unit = {
+    val x = getHello42
+    val list = List.fill(1000000)(7)
+    val newList = list.map(y => x + y)
+    hof(newList)
+  }
+
+  def branch_pure(hof: List[Int] => Unit): Unit = {
+    val x = 13
+    val y = true
+    val list = List.fill(1000000)(if (y) x else x+1 )
+    hof(list)
+  }
+
+  def branch_impure(hof: List[Int] => Unit): Unit = {
+    val x = 13
+    val y = {print("");true}
+    val list = List.fill(1000000)(if (y) x else x+1 )
+    hof(list)
+  }
+
+  def lambda(hof: List[Int] => Unit): Unit = {
+    val x = 19
+    val list = List.fill(1000000)((() => x+2)())
+    hof(list)
+  }
+
+  def fib(n: Int): Int = {
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fib(n - 2) + fib(n - 1)
+  }
+}
